d36e7c849a7ddccb82bffa4a2e44731de326f9b3
constantology <constantology@gmail.com>
2015-04-07 15:00:03 +0100

    change to use new co.wrap function (cherry picked from commit 5c3007e)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var co = require( 'co' );
 module.exports = function cothunkify( fn ) {
 	return function cothunkified( done ) {
 		var ctx = this;
-		co( function* () {
+		co.wrap( function* () {
 			yield fn.call( ctx );
 		} )( done );
 	};
